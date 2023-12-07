776b2ec6b70d6ff8f5d95e272a0453a656c13023
mateo <mateo.guenot20@gmail.com>
2023-12-08 00:28:59 +0100

    Temp: Sign up

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Home } from "./components/pages/Home"
 import { About } from "./components/pages/About"
 import {SignIn} from "./components/pages/SignIn.tsx";
+import {SignUp} from "./components/pages/SignUp.tsx";
 
 export const Router = (): JSX.Element => {
     return (
@@ -10,6 +11,7 @@ export const Router = (): JSX.Element => {
                 <Route path="/" Component={Home} />
                 <Route path="/about" Component={About} />
                 <Route path="/sign-in" Component={SignIn} />
+                <Route path="/sign-up" Component={SignUp} />
             </Routes>
         </BrowserRouter>
     )
