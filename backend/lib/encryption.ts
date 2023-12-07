d1c0506650e041ceda6fbecd2e5a0c671ece39ca
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2023-12-07 23:11:13 +0100

    route

---
+++ backend/lib/encryption.ts
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken"
+
+export const decodeJWT = (token: string) => {
+    const jwt_key = process.env.FRONT_KEY;
+    if(!jwt_key){
+        return;
+    }
+    const decoded = jwt.verify(token, jwt_key);
+    return decoded;
+}
+
+export const encodeJWT = (text: string) => {
+    const jwt_key = process.env.BACK_KEY;
+    if(!jwt_key){
+        return;
+    }
+    const encoded = jwt.sign(text, jwt_key);
+    return encoded;
+}
