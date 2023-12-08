129f5b1f4d477afc7897607994e8947801243a84
Rayane Merlin <92400806+rayanemrln@users.noreply.github.com>
2023-12-08 02:02:38 +0100

    menu + other

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -1,7 +1,45 @@
-export const Menu = () => {
+import { ChildrenInterface } from '../interface/childrenInterface';
+
+import { MenuItem } from './MenuItem';
+
+import '../assets/scss/menu.scss';
+import { useRef } from 'react';
+
+export const Menu = (props: ChildrenInterface) => {
+
+    const menu: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+
+    const handleCrossClick = () => {
+        if (menu.current) {
+            menu.current.classList.remove('open');
+        }
+    }
+
+    const handleMenuClick = () => {
+        if (menu.current) {
+            menu.current.classList.add('open');
+        }
+    }
     return (
-        <div>
-            <h1>Menu</h1>
-        </div>
+        <>
+            <div className='bars-container' onClick={ handleMenuClick }>
+                <div className='bar'></div>
+                <div className='bar'></div>
+                <div className='bar'></div>
+            </div>
+            <div className="menu" ref={ menu }>
+                <div className="close" onClick={ handleCrossClick }></div>
+                <div className='circle'></div>
+                <nav>
+                    <ul>
+                        <MenuItem to='/' text='Home'/>
+                        <MenuItem to='/post' text='Post'/>
+                        <MenuItem to='/topics' text='Topics'/>
+                        <MenuItem to='/sign-in' text='Sign In'/>
+                    </ul>
+                </nav>
+            </div>
+            { props.children }
+        </>
     );
 }
